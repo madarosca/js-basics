07637e0cb9fc9f0715d71b2c89a5b5944d909687
Madalina Rosca <madalina@neho.ch>
2023-01-21 14:24:41 +0200

    basic javascript concepts

---
+++ app.js
@@ -0,0 +1,19 @@
+function include(filePath) {
+	const scriptTag = document.createElement('script');
+	scriptTag.src = filePath;
+	document.body.appendChild(scriptTag);
+}
+
+// include('arrays.js');
+// include('constructors_and_this.js');
+// include('es6_classes.js');
+// include('functions.js');
+// include('iterators_and_generators.js');
+// include('loops.js');
+// include('numbers.js');
+// include('prototypal_inheritance.js');
+// include('prototypes.js');
+// include('strings.js');
+// include('sub_classes.js');
+// include('type_conversion.js');
+// include('window_object.js');
---
+++ iterators_and_generators.js
@@ -0,0 +1,50 @@
+// Iterator Example
+const nameIterator = (names) => {
+	let nextIndex = 0;
+
+	return {
+		next: () => (nextIndex < names.length ? { value: names[nextIndex++], done: false } : { done: true }),
+	};
+};
+
+// // Create an array of names
+const namesArr = ['Jack', 'Jill', 'John'];
+// // Init iterator and pass in the names array
+const names = nameIterator(namesArr);
+
+console.log(names.next().value);
+console.log(names.next().value);
+console.log(names.next().value);
+console.log(names.next());
+
+// Generator Example
+// function* sayNames() {
+//   yield 'Jack';
+//   yield 'Jill';
+//   yield 'John';
+// }
+
+// const sayName = sayNames();
+
+// console.log(sayName.next().value);
+// console.log(sayName.next().value);
+// console.log(sayName.next().value);
+// console.log(sayName.next().value);
+
+// ID Creator
+function* createIds(number) {
+  let index = 1;
+
+  while(index <= number) {
+    yield index++;
+  }
+}
+
+const gen = createIds(5);
+
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
